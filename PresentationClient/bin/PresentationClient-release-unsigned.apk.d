232992773590a02faa041c49d4b0d5feabb8a087
Denis Danov <denis_danof@abv.bg>
2015-03-10 21:26:15 +0200

    Commit new version

--- PresentationClient/bin/PresentationClient-release-unsigned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\x\Desktop\PresentationTool\PresentationClient\bin\PresentationClient-release-unsigned.apk : \
-C:\Users\x\Desktop\PresentationTool\PresentationClient\bin\PresentationClient.ap_ \
-C:\Users\x\Desktop\PresentationTool\PresentationClient\bin\classes.dex \
-D:\ProgramFiles\Android\Android_sdk\tools\support\annotations.jar \
